c475ca06f0857ff8b045649fd77907f6bee62062
Gerco Koks <gerco.koks@outlook.com>
2023-03-27 22:14:16 +0200

    27032023

---
+++ Snippets/ICreateGateway.cs
@@ -0,0 +1,8 @@
+﻿namespace LiquidVisions.PanthaRhei.Generator.Domain.Gateways
+{
+    public interface ICreateGateway<in TEntity>
+        where TEntity : class
+    {
+        bool Create(TEntity entity);
+    }
+}
---
+++ Snippets/SeedingBoundary.cs
@@ -0,0 +1,26 @@
+﻿internal class SeedingBoundary : ISeedingBoundary
+{
+    private readonly List<ISeederInteractor<App>> seeders;
+
+    public SeedingBoundary(IDependencyFactoryInteractor dependencyFactory)
+    {
+        seeders = dependencyFactory
+            .GetAll<ISeederInteractor<App>>()
+            .ToList();
+    }
+
+    public void Execute()
+    {
+        App app = new();
+
+        foreach (var seeder in seeders.OrderBy(x => x.ResetOrder))
+        {
+            seeder.Reset();
+        }
+
+        foreach (var seeder in seeders.OrderBy(x => x.SeedOrder))
+        {
+            seeder.Seed(app);
+        }
+    }
+}
